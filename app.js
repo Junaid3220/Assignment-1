f38c9862985c1cf01d70f4d868b66c6b4768fc28
junaid3220 <junnu3600@gmail.com>
2024-02-19 00:02:36 +0500

    Assignmet-1

---
+++ app.js
@@ -0,0 +1,156 @@
+/*Question No 1
+alert ("Welcome to Our Website");
+
+Question No 2
+document.write("Error! Please Enter a valid Password");
+
+Question No 3
+document.write("Hint: Use line break");
+
+Question No 4 
+alert("Hello World!");
+alert("Hello BanoQabil Student");
+alert("Welcome to the campus"); 
+
+Question No 5 
+alert("Please Signin"); 
+
+Question No 6 
+C 
+
+Question No 7 
+let userName;
+
+Question No 8 
+let myName;
+myName = "Muhammad Juanid"; 
+
+Question No 9 
+let titleMessage ;
+titleMessage = " Hello World!";
+alert(titleMessage);
+
+Question 10 
+let studentName = "Muhammad Junaid";
+let studentAge =   20 ;
+let studentRollno = 2055;
+let studentGrade = "B";
+let studentIspass = true;
+alert(" studentName : " + studentName);
+alert("studnetAge:  " + studentAge);
+alert("studentRollno:  " + studentRollno);
+alert("studentGrade:  " + studentGrade);
+alert("studentIspass:  " + studentIspass);
+
+Question No 11
+let email ;
+email = "Pakmail3220@gmail.com";
+alert ("Your Email Address is : " + email);
+
+Question No 12 
+document.write("This is my First Assignment ");
+
+Question No 13
+let stringValue = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(stringValue);
+document.write(stringValue);
+
+Question No 14 
+let age;
+age = 20;
+alert ("age :  " + age);
+
+Question No 15 
+let visitor ;
+visitor = "You have visit this site 2 Times";
+document.write(visitor);
+
+Question No 16 
+let birthYear;
+birthYear = 2000;
+document.write("birthYear:  " + birthYear);
+
+Question No 17 
+let visitorName = "Muhammad Junaid";
+let productTitle = "T-shirt";
+let quantity = 5 ;
+document.write(visitorName , "   ordered  " ,     quantity     ,    productTitle  , "     From  Levi's Store");
+
+Question No 18 
+let studentName = "Muhammad Junaid"  ;
+let course = "web development " ;
+let timing  = "6 to 8" ;
+document.write("studentNmae:     " , studentName ,"         course:       " ,   course , "           timing:         "  , timing );
+
+Question No 19 
+legal variable names 
+let user_Name ;
+let user$Nmae ;
+let user123Name;
+let _userName;
+let $userNmae;
+illegal variable names
+let userName# ;
+let user@Name ;
+let @userName ;
+let 123userName ;
+let user-Name ;
+
+Question No 20 
+document.write( "a) A heading stating Rules for naming JS variables  ");
+document.write( "b) Variable names can only caontain letter  underscore number or  dollar .for example $my_1stvariable " ) ;
+document.write( "c) Variable must begin with a letter underscore or dollar .for example $name _name or name  ");
+document.write( "d)  Variable names are case sensetive ");
+document.write( "e) Variable names should not be JS reserved word" ); 
+
+Question No 21 
+let a = 5;
+let b = 10;
+document.write("    a + b  " ,     a+b  );
+
+Question No 22 
+let num1 = 4 ;
+let num2 = 2 ;
+console.log( "Multiplication Result" , num1 * num2);
+console.log( "Subtraction Result" , num1 - num2);
+console.log( "Division Result" , num1 / num2);
+console.log("Modulus Result" , num1 % num2);
+
+Question No 23 
+let num1 ;
+num1 = 5 ;
+num1 = num1 + 1 ;
+num1 = num1 + 7 ;
+num1 = num1 - 1 ;
+num1 = num1 / 4 ;
+num1 = num1 % 1 ;
+console.log(num1);
+
+Question No 24 
+let movieTicket = 600 ;
+let movieTicket5 = 5 ;
+console.log(movieTicket * movieTicket5);
+
+Question No 25 
+let a = 2
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+console.log("a*num1 = " , a * num1 );
+console.log("a*num2 = " , a * num2 );
+console.log("a*num3 = " , a * num3 );
+console.log("a*num4 = " , a * num4 );
+console.log("a*num5 = " , a * num5 );
+console.log("a*num6 = " , a * num6 );
+console.log("a*num7 = " , a * num7 );
+console.log("a*num8 = " , a * num8 );
+console.log("a*num9 = " , a * num9 );
+console.log("a*num10 = " , a * num10 ); */
+
